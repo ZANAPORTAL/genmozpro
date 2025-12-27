761355f50b7155eb2232fef4be54875af4ccbdd3
ZANAPORTAL <zanaportal.com@gmail.com>
2025-12-27 22:55:03 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import { About, Privacy, Terms, Contact } from './pages/Legal';
+import { CookieConsent } from './components/CookieConsent';
+import { LanguageProvider } from './contexts/LanguageContext';
+
+const App: React.FC = () => {
+  return (
+    <LanguageProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
+          </Routes>
+        </Layout>
+        <CookieConsent />
+      </Router>
+    </LanguageProvider>
+  );
+};
+
+export default App;
